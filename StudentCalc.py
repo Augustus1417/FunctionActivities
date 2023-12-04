50c17f2c0f7e83f6119bfd15430a8a343cf74515
Augustus1417 <135231381+Augustus1417@users.noreply.github.com>
2023-12-04 11:25:52 +0800

    Update StudentCalc.py

--- StudentCalc.py
+++ StudentCalc.py
@@ -1,5 +1,5 @@
 def get_student_info(info):
-    if info == "name": return input("Enter student name: ")
+    if info == "name": return input("\nEnter student name: ")
     elif info == "subjects": return int(input("Enter number of subjects: "))
 
 def get_subject_scores(numOfSubs):
@@ -11,7 +11,7 @@ def get_subject_scores(numOfSubs):
 
 def calculate_average(scores): return (round(sum(scores)/len(scores),2))
 
-def display_result(studentName, scoreAve): return print(f"\tStudent Name: {studentName}\nAverage: {scoreAve}")
+def display_result(studentName, scoreAve): return print(f"\nStudent Name: {studentName}\nAverage: {scoreAve}")
 
 studentName = get_student_info("name")
 numOfSubs = get_student_info("subjects")
