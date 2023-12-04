5d8bb77af93a099522bd2eccf59733017458ce1b
Augustus1417 <135231381+Augustus1417@users.noreply.github.com>
2023-12-04 11:18:44 +0800

    Create StudentCalc.py

---
+++ StudentCalc.py
@@ -0,0 +1,22 @@
+def get_student_info(info):
+    if info == "name": return input("Enter student name: ")
+    elif info == "subjects": return int(input("Enter number of subjects: "))
+
+def get_subject_scores(numOfSubs):
+    scorelist = []
+    for x in range(1,numOfSubs+1):
+        score = int(input(f"Grade Score {x}: "))
+        scorelist.append(score)
+    return scorelist
+
+def calculate_average(scores): return (round(sum(scores)/len(scores),2))
+
+def display_result(studentName, scoreAve): return print(f"\tStudent Name: {studentName}\nAverage: {scoreAve}")
+
+studentName = get_student_info("name")
+numOfSubs = get_student_info("subjects")
+
+scores = get_subject_scores(numOfSubs)
+scoreAve = calculate_average(scores)
+
+display_result(studentName, scoreAve)
